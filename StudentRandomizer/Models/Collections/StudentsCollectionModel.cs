4eaf183b35c2289bb3f7f549a9ad2f77355d35f2
Michał Mikuła <mikulamichal@proton.me>
2024-01-10 09:54:21 +0100

    Add collection models

---
+++ StudentRandomizer/Models/Collections/StudentsCollectionModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRandomizer.Models.Collections
+{
+    public class StudentsCollectionModel : BaseCollectionModel<StudentModel>
+    {
+        private ObservableCollection<StudentModel> _items = new ObservableCollection<StudentModel>();
+
+        public override ObservableCollection<StudentModel> Items { get { return _items; } }
+
+        public override StudentModel GetItem(Guid id)
+        {
+            return Items.First(item => item.Id == id);
+        }
+    }
+}
