4f14e0b5251e41d3616915c1932fab93a495077b
Michał Mikuła <mikulamichal@proton.me>
2024-01-21 16:54:01 +0100

    Create StudentsViewModel

---
+++ StudentRandomizer/ViewModels/StudentsViewModel.cs
@@ -0,0 +1,47 @@
+﻿using StudentRandomizer.Models;
+using StudentRandomizer.Models.Collections;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRandomizer.ViewModels
+{
+    public class StudentsViewModel : INotifyPropertyChanged
+    {
+        private StudentsCollectionModel _collectionModel;
+
+        private ObservableCollection<StudentModel> _models;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<StudentModel> Models
+        {
+            get => _models;
+            set => SetProperty(ref _models, value, nameof(Models));
+        }
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        private bool SetProperty<T>(ref T property, T value, [CallerMemberName] string propertyName = "")
+        {
+            if(Object.Equals(property, value))
+            {
+                return false;
+            }
+
+            property = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public StudentsViewModel(StudentsCollectionModel studentsCollectionModel)
+        {
+            this._collectionModel = studentsCollectionModel;
+            this._models = _collectionModel.Items;
+        }
+    }
+}
