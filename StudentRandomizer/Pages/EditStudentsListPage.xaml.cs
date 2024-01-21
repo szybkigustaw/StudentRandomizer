ad3936ff359ddea492f5d4ab656b59338c1c00a0
Michał Mikuła <mikulamichal@proton.me>
2024-01-21 22:46:53 +0100

    Move BindingContext to the code-behind file.

--- StudentRandomizer/Pages/EditStudentsListPage.xaml.cs
+++ StudentRandomizer/Pages/EditStudentsListPage.xaml.cs
@@ -1,9 +1,14 @@
+using StudentRandomizer.ViewModels;
+
 namespace StudentRandomizer.Pages;
 
 public partial class EditStudentsListPage : ContentPage
 {
+	private StudentsViewModel _viewmodel;
 	public EditStudentsListPage()
 	{
 		InitializeComponent();
+		_viewmodel = this.Handler.MauiContext.Services.GetService<StudentsViewModel>();
+		this.BindingContext = _viewmodel;
 	}
 }
